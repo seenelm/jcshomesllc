5b47f841538b9e8ae4547c3bac82c291a332e310
Yassine Elmellouki <yelmellouki@gmail.com>
2024-05-22 23:57:35 -0400

    added in branding

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,16 @@
-import './style.css';
-
-// Function to load HTML template and insert it into the DOM
-async function loadComponent(componentId: string, containerId: string) {
-  try {
-    const response = await fetch(`src/components/${componentId}.html`);
-    const data = await response.text();
-    const template = document.createElement('template');
-    template.innerHTML = data.trim();
-    const templateContent = (template.content.querySelector(`#${componentId}-template`) as HTMLTemplateElement).content.cloneNode(true);
-    document.getElementById(containerId)!.appendChild(templateContent);
-  } catch (error) {
-    console.error('Error loading component:', error);
-  }
-}
+import './styles/style.css';
+import { loadComponent } from './utils/loadComponent';
 
 loadComponent('header', 'header-container');
 loadComponent('hero', 'hero-container');
+loadComponent('about', 'about-container');
+loadComponent('projects', 'projects-container');
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <div id="header-container"></div>
     <div id="hero-container"></div>
+    <div id="about-container"></div>
+    <div id="projects-container"></div>
   </div>
 `;
