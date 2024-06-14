193ab69a02f253eef22f985f754bd32eb5f64a25
Yassine Elmellouki <yelmellouki@gmail.com>
2024-06-13 20:57:06 -0400

    big changes

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
-import { loadComponent } from './utils/loadComponent';
+
 import './styles/header.css';
 import './styles/hero.css';
 import './styles/whyus.css';
 import './styles/about.css';
 import './styles/style.css';
 
+
+export async function loadComponent(componentId: string, containerId: string): Promise<void> {
+  try {
+    // Adjust the path as necessary to match your project structure
+    const response = await fetch(`/src/components/${componentId}.html`);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch component ${componentId}: ${response.statusText}`);
+    }
+    const data = await response.text();
+    const template = document.createElement('template');
+    template.innerHTML = data.trim();
+    const templateContent = (template.content.querySelector(`#${componentId}-template`) as HTMLTemplateElement).content.cloneNode(true);
+    document.getElementById(containerId)!.appendChild(templateContent);
+  } catch (error) {
+    console.error(`Error loading component ${componentId}:`, error);
+  }
+}
 
 loadComponent('header', 'header-container');
 loadComponent('hero', 'hero-container');
